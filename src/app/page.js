c37ba91dfd71271ca8232e128623b60e7a6aaf75
Sham Roy <rs265622@gmail.com>
2025-06-08 12:57:09 +0530

    3.js fixes

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import "./page.scss";
-
 import Header from '../components/Header/Header';
 import ProfileSummary from '../components/ProfileSummary/ProfileSummary';
 import Skills from '../components/Skills/Skills';
@@ -9,27 +8,28 @@ import Projects from '../components/Projects/Projects';
 // import EducationAndCert from '../components/EducationAndCert/EducationAndCert'; // Updated
 import Languages from '../components/Languages/Languages';
 import Footer from '../components/Footer/Footer';
-import Education from '../components/Education/Education'; // Renamed
+import Education from '../components/Education/Education'; 
 import Certification from '../components/Certification/Certification';
-import AboutMe from '../components/AboutMe/AboutMe'; // New
-import Contact from '../components/Contact/Contact'; // Import
+import AboutMe from '../components/AboutMe/AboutMe';
+import Contact from '../components/Contact/Contact'; 
 import TechStackBanner from '../components/TechStackBanner/TechStackBanner';
 import ThreeJsBackground from '@components/components/ThreeJsBackground/ThreeJsBackground';
-
+import HeroSection from '../components/HeroSection/HeroSection';
 
 export default function Home() {
   return (
     <div className="portfolio-app">
       <Header />
+      <HeroSection />
       <main>
-        <ProfileSummary />
+        <ProfileSummary id="profile" />
         <AboutMe />
         <Skills />
         <Experience />
-        <Projects />
-        <Contact />
-        {/* <Education /> 
-        <Certification />  */}
+        <Projects id="projects" />
+        <Contact id="contact" />
+        {/* <Education />  */}
+        {/* <Certification />   */}
         <Languages />
         <TechStackBanner />
       </main>
